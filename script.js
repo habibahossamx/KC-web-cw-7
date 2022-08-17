0fc8000e9d4609a8464a3472536031c6a2058015
habibahossamx <109871458+habibahossamx@users.noreply.github.com>
2022-08-17 23:21:23 +0300

    تعديل

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
-function BMI(w, h) {
-let result= w / (h*h)
-return result
+function BMI(weight,height) {
+let result= weight / (height*height)
+return result ;
 }
 
-console.log(BMI (10,10))
+console.log(BMI (40,1.6));
 
 
 function Status(BMI) {
 if (BMI <18.5) {
-return "لديك نقص في الوزن"
+return ("لديك نقص في الوزن");
 
 }else if (BMI >=18.5  &&BMI <25 ){
-return"وزنك صحي"
+return("وزنك صحي");
 
-}else if(BMI >=25) {
-    "لديك زياده في الوزن "
+}else  {
+    return ("لديك زياده في الوزن ");
 }
 
 }
 
 function calculate(){
-    let weight=document.getElementById("weight").value 
-    let height=document.getElementById("height").value 
+    let weight=document.getElementById("weight").value ;
+    let height=document.getElementById("height").value ;
 
-    let BMI= BMI(weight,height)
-    let desc =  Status (BMI)
-    let div =document.getElementById(result)
-    div.innerTEXT = BMI + " == " + desc 
+    let BMI= BMI(weight,height);
+    let desc =  Status (BMI);
+    let div =document.getElementById("result");
+    div.innerTEXT = BMI + " == " + desc ;
 }
